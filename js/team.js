62df8abac53b5da9138291bfaaa5d5d9aa25b701
Yury Lifshits <yury@yury.name>
2017-01-04 07:31:20 -0800

    New photos

--- js/team.js
+++ js/team.js
@@ -8,7 +8,7 @@ var teamMembers = [
 	},
 	{
 		name: 'Mat Frenz',
-		photo: 'img/team/06.png',
+		photo: 'img/team/mat.png',
 		post: 'Director of Operations',
 		linkedin: 'https://www.linkedin.com/pub/mathew-frenz/9/7b6/586',
 		bio: '<p><strong>Mat Frenz</strong> is the Director of Operations at Entangled Solutions. He is dedicated to ensuring that the work we produce at Entangled is not only best-in-class, but is also carried out with professionalism, courtesy and integrity.</p><p>Prior to joining Entangled, Mat led platform operations for GlassLab, a non-profit educational technology organization funded by the Gates Foundation and the MacArthur Foundation. There, he was responsible for GlassLab Services, leading product, business and partnership strategy while driving an industry-leading approach to developing, analyzing, and distributing educational games. Mat was also the founder of Precision Response, a software as a service company devoted to supporting nonprofit organizations in their efforts to extend their reach and fundraising capabilities.</p>'
@@ -29,14 +29,14 @@ var teamMembers = [
 	},
 	{
 		name: 'Deborah Seymour',
-		photo: 'http://placehold.it/230x230',
+		photo: 'img/team/deborah.png',
 		post: 'Principal Consultant',
 		linkedin: 'https://www.linkedin.com/in/deborahmseymour',
 		bio: '<p><strong>Deborah Seymour</strong> is a Principal Consultant at Entangled Solutions. Previously, she served as chief academic innovation officer at the American Council on Education, a role in which she oversaw the Council’s agenda to support the return of the United States to global recognition in post-secondary attainment.</p><p>Deborah taught in the City University of New York system for 15 years. She also served as associate director of the Center for Adult Life & Learning at the 92nd Street Y in New York City, with responsibility for adult education programs and strategic partnerships with multiple external organizations. Deborah taught as well at the University of California-Irvine (UCI) and at Orange Coast College. She subsequently became a development officer for the School of Humanities at UCI, before transitioning to Laureate Education. At Laureate, she worked in all facets of product development and product management, both domestically and internationally. Her Laureate career also included research in the area of competency-based education. Deborah holds a BA from Hunter College in business and communications and an MPhil and PhD in theoretical linguistics from the City University of New York Graduate School and University Center.</p>'
 	},
 	{
 		name: 'Mike Berlin',
-		photo: 'http://placehold.it/230x230',
+		photo: 'img/team/mike.png',
 		post: 'Principal Consultant',
 		linkedin: 'https://www.linkedin.com/in/berlinmike',
 		bio: '<p><strong>Mike Berlin</strong> is a Principal Consultant at Entangled Solutions. Mike has a ten-year career investing in, acquiring, advising, and building startups in the Higher Education market. He currently acts as an advisor and mentor to several edtech startups.</p><p>Prior to joining Entangled Solutions, Mike led strategy and M&A at Macmillan New Ventures, global publisher Macmillan’s education innovation division. Previously, Mike has worked as a consultant at L.E.K. Consulting. He has a B.S. in Economics from the Wharton School at the University of Pennsylvania.</p>'
@@ -50,42 +50,42 @@ var teamMembers = [
 	},
 	{
 		name: 'Amber Laxton',
-		photo: 'http://placehold.it/230x230',
+		photo: 'img/team/amber.png',
 		post: 'Consulting Team Leader',
 		linkedin: 'https://www.linkedin.com/in/amberlaxton',
 		bio: '<p><strong>Amber Laxton</strong> is a Consulting Team Leader at Entangled Solutions. She joins Entangled with over eight years of experience in continuing education, most recently as a Senior Analyst of Continuing Education at Eduventures. She previously worked as the Director of Academic Services at the University of California Santa Cruz Extension and the Enrollment Services Manager at DePaul University Continuing and Professional Education. She has experience developing and managing non-credit and non-degree programs and optimizing technology to improve operational efficiency and student services.</p>'
 	},
 	{
 		name: 'Jasmin Schiener',
-		photo: 'http://placehold.it/230x230',
+		photo: 'img/team/jasmin.png',
 		post: 'Consulting Team Leader',
 		linkedin: 'https://www.linkedin.com/in/jasminschiener',
 		bio: '<p><strong>Jasmin Schiener</strong> is a Consulting Team Leader at Entangled Solutions. She joins Entangled with deep experience in Higher Education management, most recently as a project manager for a fast-growing for-profit education organization. She previously ran the San Francisco division of a recruiting startup focused on the Bay Area tech industry. Jasmin earned a law degree at Bucerius Law school in Germany, and is experienced in program development, market analysis, M&A, healthcare education and the creation of sustainable operations infrastructure for highly regulated education entities.</p>'
 	},
 	{
 		name: 'Lauren Dibble',
-		photo: 'http://placehold.it/230x230',
+		photo: 'img/team/lauren.png',
 		post: 'Consulting Team Leader',
 		linkedin: 'https://www.linkedin.com/in/laurendibble',
 		bio: '<p><strong>Lauren Dibble</strong> is a Consulting Team Leader at Entangled Solutions, currently focused on Matterfund, an education funding platform in development on behalf of the World Economic Forum.</p><p>Prior to joining Entangled Solutions, Lauren was the founding Program Manager of Northeastern University\'s Venture Mentoring Network and led marketing, communications, and strategic initiatives for the Northeastern University Center for Entrepreneurship Education. Lauren worked closely with student- and alumni-founded startups and organized university-wide programs and events.</p><p>Previously, Lauren was a member of the Media Relations team at Northeastern University, after working with B2B technology clients at Weber Shandwick, a leading global public relations firms.</p>'
 	},
 	{
 		name: 'Kyle Cureau',
-		photo: 'http://placehold.it/230x230',
+		photo: 'img/team/kyle.png',
 		post: 'Director of Strategic Initiatives',
 		linkedin: 'https://www.linkedin.com/in/cureau',
 		bio: '<p><strong>Kyle Cureau</strong> is the Director of Strategic Initiatives at Entangled Solutions, currently leading Matterfund, the first real-time graph intelligence platform for international education development. He has consulted across verticals, leading teams to design and develop business models, system architecture, APIs, web apps, and mobile apps.</p><p>He also co-founded the health charity LzA to provide supplies to malnourished children. LzA, in partnership with CORE, PacSun, AmFam, and Birchbox, helped kids and communities in Central America. While still in college, Kyle started his first venture, BuyWell, a Fair Trade coffee roaster and wholesaler. Within three years, he had grown BuyWell to a team of 15, working directly with farming coops in seven countries, and roasting 15,000lbs a month for major retailers.</p><p>With a career bent for impact and a passion for education, Kyle\'s focus is on student experience. He believes strongly in learning-by-doing and learning-by-playing.</p>'
 	},
 	{
 		name: 'Casey Boyles',
-		photo: 'http://placehold.it/230x230',
+		photo: 'img/team/casey.png',
 		post: 'Partnerships Manager',
 		linkedin: 'https://www.linkedin.com/in/caseythomasboyles',
 		bio: '<p><strong>Casey</strong> is the Partnerships Manager at Entangled Solutions. Prior to joining the team she spent the last 4 years working with Bay Area startups, helping to build sales, customer success and customer support teams. She has experience building summer school programs and has a passion for continuing education.</p><p>Casey earned a degree in Physics at King’s College London, a masters degree in Nanotechnology from the University of Pennsylvania and spent a year doing research on Electrical & Computer Engineering at the National University of Singapore.</p>'
 	},
 	{
 		name: 'Faz Nassiri',
-		photo: 'http://placehold.it/230x230',
+		photo: 'img/team/faz.png',
 		post: 'Operations Manager',
 		linkedin: 'https://www.linkedin.com/in/fazilatnassiri',
 		bio: 'TBD'
